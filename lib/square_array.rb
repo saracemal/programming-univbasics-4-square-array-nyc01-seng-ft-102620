392d3bbee3383a9db4d98339d1aaad42ad2fed1a
Sara Cemal <saracemal@gmail.com>
2020-10-23 05:10:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   squared = []
   array.length { |element| squared << element ** 2 }
   count = 0
-  while count < array.size do
+  while count < array.length do
     squared << (array[count])
     count = count + 1
   end
